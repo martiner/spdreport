341ab17809556aae721ce8822762c4638631114f
Martin Caslavsky <mcaslavsky@gmail.com>
2023-05-09 11:03:49 +0200

    initial

---
+++ src/main/kotlin/cz/geek/spdreport/SpdreportApplication.kt
@@ -0,0 +1,11 @@
+package cz.geek.spdreport
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpdreportApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpdreportApplication>(*args)
+}
---
+++ src/test/kotlin/cz/geek/spdreport/SpdreportApplicationTests.kt
@@ -0,0 +1,13 @@
+package cz.geek.spdreport
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpdreportApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
