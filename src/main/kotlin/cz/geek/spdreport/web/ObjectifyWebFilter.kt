94ce356f45e73095ef115479fdfb6a0bd2d834d3
Martin Caslavsky <mcaslavsky@gmail.com>
2025-01-10 19:42:12 +0100

    chg: update dependencies

--- src/main/kotlin/cz/geek/spdreport/web/ObjectifyWebFilter.kt
+++ src/main/kotlin/cz/geek/spdreport/web/ObjectifyWebFilter.kt
@@ -1,16 +1,5 @@
 package cz.geek.spdreport.web
 
 import com.googlecode.objectify.ObjectifyService
-import jakarta.servlet.Filter
-import jakarta.servlet.FilterChain
-import jakarta.servlet.ServletRequest
-import jakarta.servlet.ServletResponse
 
-class ObjectifyWebFilter : Filter {
-
-    override fun doFilter(req: ServletRequest, resp: ServletResponse, chain: FilterChain) =
-        ObjectifyService.begin()
-            .use {
-                chain.doFilter(req, resp)
-            }
-}
+class ObjectifyWebFilter : ObjectifyService.Filter()
