0ab6834286fd0c8c27e49ac02223756937eab611
Martin Caslavsky <mcaslavsky@gmail.com>
2025-02-13 14:15:42 +0100

    add: show cron schedule in settings

--- src/main/kotlin/cz/geek/spdreport/model/EmailFrequency.kt
+++ src/main/kotlin/cz/geek/spdreport/model/EmailFrequency.kt
@@ -1,6 +1,12 @@
 package cz.geek.spdreport.model
 
+import org.springframework.core.io.ClassPathResource
+import org.yaml.snakeyaml.LoaderOptions
+import org.yaml.snakeyaml.Yaml
+import org.yaml.snakeyaml.constructor.Constructor
+import java.nio.file.Path
 import java.time.LocalDate
+import kotlin.io.path.name
 
 enum class EmailFrequency {
     NONE {
@@ -28,4 +34,32 @@ enum class EmailFrequency {
 
     val title: String
         get() = name.lowercase()
+
+    companion object {
+        val SCHEDULE: Map<EmailFrequency, String?>  =
+            Yaml(Constructor(CronConfig::class.java, LoaderOptions()))
+                .run {
+                    load<CronConfig>(ClassPathResource("cron.yaml").inputStream)
+                }
+                .run {
+                    cron.mapNotNull { it.asPair() }.toMap()
+                }
+                .run {
+                    EmailFrequency.entries.associateWith { this[it.name] }
+                }
+    }
+}
+
+data class CronConfig(
+    var cron: List<CronJob> = listOf(),
+)
+
+data class CronJob(
+    var url: String? = null,
+    var schedule: String? = null,
+    var timezone: String? = null,
+    var description: String? = null,
+) {
+    fun asPair(): Pair<String, String>? =
+        if (url != null && schedule != null) (Path.of(url!!).name.uppercase() to schedule!!) else null
 }
